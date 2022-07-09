1c863c379412ff50c84a9826d130a4417c99b0ef
choyondas <choyondas08@gmail.com>
2022-07-09 19:10:34 +0800

    added nav

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import './Nav.css';
+
+const Nav = () => {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg bg-light py-3">
+          <div className="container-fluid">
+            <h2>DasShop</h2>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Shop
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </li>
+                <li className="nav-item">
+                  <i class="fa-solid fa-cart-shopping"></i>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+};
+
+export default Nav;
